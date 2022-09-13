fd05ac8c4de0ed535082f0fcce108ef40b69feba
Marcelo-MV <marcelomventura@hotmail.com>
2022-09-13 00:05:24 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+//objeto mochila de um aluno
+const mochilaAluno = [
+    'cartão de crédito',
+    'chaves',
+    'dinheiro',
+    'escova de dente',
+    'alcool gel'
+]
+//pega o ID do elemento no html
+let resultado = document.getElementById('incrementar');
+//laço de repetição que percorre o indice da variável mochilaAluno
+for (let index = 0; index < mochilaAluno.length; index++ ){
+    // item pega o indice atual da iteração e coloca na posição dentro da mochilaAluno
+   let item = mochilaAluno[index]
+    console.log(item)
+    //cria um elemento <p></p> a cada interação
+    let criar = (document.createElement("p")) 
+    //dentro deste elemento criado é alterado a propriedade innerHTML com o nome do 
+    criar.innerHTML= item
+    //acrescenta como filho direto o elemento criado com os atributos constituidos
+    resultado.appendChild(criar)
+}
